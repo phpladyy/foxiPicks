0b0df411966c2f078e1b1687c3c9de5f5320565f
phpladyy <eluwina08325@gmail.com>
2026-06-29 15:05:36 +0200

    cleaned up app component

--- src/updateWatched.js
+++ src/updateWatched.js
@@ -1,12 +1,9 @@
-import { useState } from "react";
-
-export async function updateWatched(session, watched,operation,movie,id) {
-
+export async function updateWatched(session, watched, operation, movie, id) {
   const res = await fetch(`/.netlify/functions/${operation}`, {
     method: "POST",
-    body: JSON.stringify({ session, watched}),
+    body: JSON.stringify({ session, watched }),
   });
- 
+
   const data = await res.json();
   if (data.error) {
     throw new Error(data.error);
